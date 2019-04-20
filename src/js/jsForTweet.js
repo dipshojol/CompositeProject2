4759b49d17c139df10e7cc5f9559dab68a819055
shojol <dip_shojol@yahoo.com>
2019-04-20 13:19:30 -0400

    some error fixes and working on filter

--- src/js/jsForTweet.js
+++ src/js/jsForTweet.js
@@ -1,35 +1,43 @@
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
-const myTweet = document.getElementById('tweet-text');
-const ul = document.querySelector('ul');
+"use strict";
 
-const username = localStorage.getItem("Email");
+jQuery(document).ready(() => {
 
-const liMaker = (text) => {
-    const li = `<li class='inner-li row'><div class='avatar col-1'><div class="inner-avatar"><img src="../../assets/twitter-avatar.png" alt=""></div></div><div class='user-name col-11'><h6>${username}</h6><p class='col-11'>${text}</p></div></li>`;
-
-    jQuery('#tweeted').append(li);
-}
-
-jQuery('.tweet-btn').on('click', (e) => {
-    e.preventDefault();
-
-    itemsArray.push(myTweet.value);
+    let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
     localStorage.setItem('items', JSON.stringify(itemsArray));
-    liMaker(myTweet.value);
-    console.log(`${itemsArray}`);
-    myTweet.value = "";
-});
+    const data = JSON.parse(localStorage.getItem('items'));
+    const myTweet = document.getElementById('tweet-text');
+    const ul = document.querySelector('ul');
 
-data.forEach(item => {
-    liMaker(item);
-});
+    const username = localStorage.getItem("Email");
 
-jQuery('.clear-btn').on('click', () => {
-    localStorage.clear();
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
+    const liMaker = (text) => {
+        const li = `<li class='inner-li row'><div class='avatar col-1'><div class="inner-avatar"><img src="../../assets/twitter-avatar.png" alt=""></div></div><div class='user-name col-11'><h6>${username}</h6><p class='col-11'>${text}</p></div></li>`;
+
+        jQuery('#tweeted').append(li);
     }
-    itemsArray = [];
+
+    jQuery('.tweet-btn').on('click', (e) => {
+        e.preventDefault();
+
+        if (myTweet.value) {
+            itemsArray.push(myTweet.value);
+            localStorage.setItem('items', JSON.stringify(itemsArray));
+            liMaker(myTweet.value);
+            // console.log(`${itemsArray}`);
+            myTweet.value = "";
+        }
+    });
+
+    data.forEach(item => {
+        liMaker(item);
+    });
+
+    jQuery('.clear-btn').on('click', () => {
+        localStorage.clear();
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+        itemsArray = [];
+    });
+
 });
